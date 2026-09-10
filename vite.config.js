4eba9d47d4c0757883e415c11fa27cac58963c74
barsam1999 <barsam.bagheri99@gmail.com>
2026-09-10 16:08:04 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/whitewingsrentcar/",
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
   server: {
     host: "0.0.0.0",
     port: 3000,
